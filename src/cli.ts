79414eb004408ef93298c59dbf8f36fd738e72dc
Yuan Qing Lim <desk@yuanqing.sg>
2022-09-12 22:10:54 -0600

    Add description

--- src/cli.ts
+++ src/cli.ts
@@ -4,13 +4,17 @@ import sade from 'sade'
 import { serveAsync } from './serve-async.js'
 
 sade('stable-diffusion-rest-api', true)
-  .option('-c, --cert', '', './cert.pem')
-  .option('-k, --key', '', './key.pem')
-  .option('-m, --model', '', './model.ckpt')
-  .option('-o, --output', '', './output')
-  .option('-p, --port', '', 8888)
-  .option('-s, --seed', '', 42)
-  .option('-sd, --stable-diffusion-directory', '', './stable-diffusion')
+  .option('-c, --cert', 'Path to the SSL certicate', './cert.pem')
+  .option('-k, --key', 'Path to the SSL certicate key', './key.pem')
+  .option('-m, --model', 'Path to the Stable Diffusion model', './model.ckpt')
+  .option('-o, --output', 'Directory to output generated images', './output')
+  .option('-p, --port', 'Port to serve the REST API', 8888)
+  .option('-s, --seed', 'Default seed', 42)
+  .option(
+    '-sd, --stable-diffusion-repository',
+    'Path to the Stable Diffusion repository',
+    './stable-diffusion'
+  )
   .action(async function (options: {
     cert: string
     key: string
